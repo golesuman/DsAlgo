c5c28f99d5af472324318834dc4c51d68b734549
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-02-09 17:57:19 +0545

    Add files via upload

---
+++ Sorting Algorithm/SelectionSort/SelectionSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int i, j, n;
+     cin >> n;
+     int a[n];
+     for (i = 0; i < n; i++)
+     {
+          cin >> a[i];
+     }
+     for (i = 0; i < n - 1; i++)
+     {
+          for (j = i + 1; j< n; j++)
+          {
+              
+               if (a[j] < a[i])
+               {
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
+               }
+          }
+     }
+     for (i = 0; i < n; i++)
+     {
+          cout << a[i] << endl;
+     }
+}
