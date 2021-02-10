4f2d7349bf77fbdf4530f87a00f8684b094f1679
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-02-10 17:26:42 +0545

    Add files via upload

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int Sum( int n)
+{
+     if(n==0)
+     {
+          return 0;
+     }
+     else{
+          return n+Sum(n-1);
+     }
+}
+int main()
+{
+int n;
+cin>>n;
+cout<<Sum(n);
+}
