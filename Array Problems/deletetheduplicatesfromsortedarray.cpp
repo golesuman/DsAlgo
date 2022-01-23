b5954a10a6555e1b2e2a14fbc0b0be581ac5b8c4
Suman <gole35830@gmail.com>
2022-01-23 19:07:06 +0545

    Delete duplicates
    Delete the duplicates from the sorted array

---
+++ Array Problems/deletetheduplicatesfromsortedarray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int removeDuplicates(int a[],int n)
+{
+    int len=0;
+    
+    if(n==0)
+            cout<<"Empty array"<<endl;
+    else
+     {      for(int i=0;i<n-1;i++){
+        
+            if(a[i]!=a[i+1])
+            {
+                a[len]=a[i];
+                len++;
+
+            }
+           
+        }
+    a[len++]=a[n-1];
+    return len;
+}
+}
+int main()
+{
+    int arr[]={1,1,1,2,2,3,4};
+    int n=sizeof(arr)/sizeof(int);
+    int len=removeDuplicates(arr,n);
+    int i;
+    cout<<"Array after removing duplicates:"<<endl;
+    for(i=0;i<len;i++)
+    {   
+        cout<<arr[i]<<endl;
+
+    }
+}
