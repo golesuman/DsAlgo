1f82978ba2891dcfff3f947429e844c57ee86c17
golesuman <gole35830@gmail.com>
2022-09-04 19:23:39 +0545

    add : triplet problem

---
+++ New_Start/Array_problems/triplet.cpp
@@ -0,0 +1,57 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+vector<vector<int>> triplets(vector<int>arr, int sum)
+{
+    sort(arr.begin(), arr.end());
+    vector<vector<int> > result;
+    int n = arr.size();
+    for(int i= 0;i<=n-3; i++)
+    {
+        int j = i+1;
+        int k = n-1;
+         
+        //two pointer approach
+        while(j<k){
+            int current_sum = arr[i];
+            current_sum += arr[j];
+            current_sum += arr[k];
+            
+            if(current_sum == sum)
+            {
+                result.push_back({arr[i], arr[j], arr[k]});
+                j++;
+                k--;
+            }
+            else if(current_sum > sum)
+            {
+                k--;
+            }
+            else{
+                j++;
+            }
+        }        
+
+    }
+    return result;
+}
+int main()
+{
+    vector<int>arr={1, 2, 3, 4, 5, 6, 7, 8, 9, 15};
+    int S = 18;
+    auto result = triplets(arr, S);
+    for(auto v: result)
+    {
+        for(auto no : v)
+        {
+            cout<<no<<",";
+        }
+        cout<<endl;
+    }
+
+
+
+}
